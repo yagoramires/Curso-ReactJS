515b6961d2d683a53211f3a67ca8454114f2a807
D2B <suporte@d2bonline.com.br>
2022-08-08 14:52:34 -0300

    login page and function

--- Seção 10 - Projeto Miniblog/miniblog/src/hooks/useAuth.js
+++ Seção 10 - Projeto Miniblog/miniblog/src/hooks/useAuth.js
@@ -46,8 +46,8 @@ export const useAuth = () => {
 
       return user;
     } catch (error) {
-      console.log(error.message);
-      console.log(typeof error.message);
+      // console.log(error.message);
+      // console.log(typeof error.message);
 
       let systemErrorMessage;
 
@@ -59,19 +59,46 @@ export const useAuth = () => {
         systemErrorMessage = 'Ocorreu um erro, tente novamente mais tarde.';
       }
       setError(systemErrorMessage);
+      setLoading(false);
     }
   };
 
-  // logout
+  // logout - sign out
 
   const logout = () => {
     checkIfIsCanceled();
     signOut(auth);
   };
 
+  // login - sign in
+
+  const login = async (data) => {
+    checkIfIsCanceled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes('user-not-found')) {
+        systemErrorMessage = 'Usuário não encontrado.';
+      } else if (error.message.includes('wrong-password')) {
+        systemErrorMessage = 'Senha incorreta.';
+      } else {
+        systemErrorMessage = 'Ocorreu um erro, tente novamente mais tarde.';
+      }
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     return () => setCancelled(true);
   }, []);
 
-  return { auth, createUser, error, loading, logout };
+  return { auth, createUser, error, loading, logout, login };
 };
--- Seção 10 - Projeto Miniblog/miniblog/src/pages/Login/Login.jsx
+++ Seção 10 - Projeto Miniblog/miniblog/src/pages/Login/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import styles from './Login.module.css';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-export default Login
+  const { login, error: authError, loading } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError('');
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await login(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+  return (
+    <section className={styles.loginContainer}>
+      <h1>Entrar</h1>
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span> E-mail:</span>
+          <input
+            type='email'
+            required
+            placeholder='Insira seu e-mail'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span> Senha:</span>
+          <input
+            type='password'
+            required
+            placeholder='Insira sua senha'
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        {!loading && (
+          <button type='submit' value='Registrar' className='btn btn-dark'>
+            Cadastrar
+          </button>
+        )}
+        {loading && (
+          <button type='submit' value='Registrar' className='btn' disabled>
+            Aguarde ...
+          </button>
+        )}
+        {error && <p className='error'>{error}</p>}
+      </form>
+    </section>
+  );
+};
+
+export default Login;
