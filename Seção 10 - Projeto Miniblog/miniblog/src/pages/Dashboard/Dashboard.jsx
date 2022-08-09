383c423329c42d795c7d69e78e64dfccf6f55368
D2B <suporte@d2bonline.com.br>
2022-08-09 14:19:15 -0300

    post delete

--- Seção 10 - Projeto Miniblog/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ Seção 10 - Projeto Miniblog/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { useAuthValue } from '../../context/AuthContext';
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useDeleteDocument } from '../../hooks/useDeleteDocument';
 
 import styles from './Dashboard.module.css';
 
@@ -13,16 +14,16 @@ const Dashboard = () => {
 
   // user posts
   const { documents: posts, loading } = useFetchDocuments('posts', null, uid);
-  // const posts = []
 
   // delete
-  const handleDelete = () => {};
+  const { deleteDocument } = useDeleteDocument('posts');
+
+  if (loading) return <p>Carregando ...</p>;
 
   return (
     <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
-      {loading && <p>Carregando posts</p>}
       {posts && posts.length === 0 ? (
         <div className={styles.noposts}>
           <p>Não foram encontrados posts</p>
@@ -50,12 +51,22 @@ const Dashboard = () => {
                   >
                     Editar
                   </Link>
-                  <button
-                    onClick={handleDelete}
-                    className='btn btn-outline btn-danger'
-                  >
-                    Excluir
-                  </button>
+                  {loading ? (
+                    <button
+                      disabled
+                      onClick={() => deleteDocument(post.id)}
+                      className='btn btn-outline btn-danger'
+                    >
+                      Excluindo
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => deleteDocument(post.id)}
+                      className='btn btn-outline btn-danger'
+                    >
+                      Excluir
+                    </button>
+                  )}
                 </div>
               </div>
             ))}
