791e38e7e907ae2083dbd4431895a2163d743526
D2B <suporte@d2bonline.com.br>
2022-08-15 10:41:17 -0300

    useReducer

--- Seção 11 - React Hooks/hooks/src/App.js
+++ Seção 11 - React Hooks/hooks/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Seção 11 - React Hooks/hooks/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HookUseReducer from '../components/HookUseReducer'
+
+const Home = () => {
+  return (
+    <div>
+      <HookUseReducer/>
+    </div>
+  )
+}
+
+export default Home
